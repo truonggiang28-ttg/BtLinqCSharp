c188c91d736056665799fe8f2b4d2d1acee08cf0
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 14:20:00 +0700

    em nop bai 19 kem anhr code va kq a

---
+++ Bt19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Bt19
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+
+            List<int> list = new List<int>();
+            list.AddRange(1, 2, 2, 3, 4, 4, 5);
+            Console.Write("Danh sach la: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n----------------------------------------\n");
+            var listGroup = list.GroupBy(n => n);
+            var countMax = listGroup.Max(num => num.Count() );
+            var resultGroup = listGroup.Where(n => n.Count() == countMax);
+            foreach(var i in resultGroup)
+            {
+                Console.WriteLine($"So xuat hien nhieu nhat la {i.Key} xuat hien {countMax} lan"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
