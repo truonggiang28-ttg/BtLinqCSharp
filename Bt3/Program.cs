07255a98cada434730a48149c2b3fa499223d2b9
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 13:56:21 +0700

    hoan thanh bai 3 file chua anh

---
+++ Bt3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Bt3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+            List<int> list = new List<int>();
+            Console.Write("Nhap so luong phan tu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Phan tu thu {i+1}: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                list.Add(num);
+            }
+            var listResult = from num in list
+                             orderby num
+                             select num;
+            Console.WriteLine("\n----------------------------------------\n");
+            Console.Write("Day so nguyen sau khi sap xep tang dan: ");
+            foreach(int j in listResult)
+            {
+                Console.Write($"{j} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
