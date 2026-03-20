3f41bc6837dc8e778fe6ea0141c08f4b4132be59
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 14:15:19 +0700

    bai 15 em nop co anh kq va code a

---
+++ Bt15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Bt15
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+
+            List<int> list = new List<int>();
+            list.AddRange(1, 2, 2, 3, 4, 4, 5);
+            Console.Write("Danh sach la: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " "); 
+            }
+
+            var listResult = list.Distinct();
+            Console.Write("\n\nDanh sach khong chua so trung la: ");
+            foreach(int i in listResult)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("\n----------------------------------------\n");
+            Console.ReadKey();
+        }
+    }
+}
