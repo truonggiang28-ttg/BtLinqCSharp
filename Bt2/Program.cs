14a486ac95fc212db42e125dfb1346bdb4d660b3
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 13:54:35 +0700

    nop bai 2 file chua anh chup man hinh

---
+++ Bt2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Bt2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+
+            Console.Write("Danh sach da nhap la: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            var listResult = list.Where(i => i > 5);
+            Console.WriteLine("\n----------------------------------------");
+            Console.Write("Cac so lon hon 5 co trong list la: ");
+            foreach (var item in listResult)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
