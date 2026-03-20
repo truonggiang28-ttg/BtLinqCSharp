26d17b8f43713f5af2791c1940f8ca5c5753532e
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 14:16:38 +0700

    em  nop bai 16 co anh result va code

---
+++ Bt16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Bt16
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+
+            List<int> list = new List<int>();
+            list.AddRange(1, 2, 2, 3, 4, 4, 5);
+            Console.Write("Danh sach la: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            var listResult = list.Sum();
+            Console.Write($"\n\nTong cua day la: {listResult}\n");
+            Console.ReadKey();
+
+        }
+    }
+}
