0fd93861d189688eb2a28ad8cdc901ed81561541
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-20 14:17:59 +0700

    thua thay e nop bai 17 co anh kq va code

---
+++ Bt17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BaiTap
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+    }
+    public class Bt17
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313_225LTC#02");
+            Console.WriteLine("----------------------------------------\n");
+            List<Student> students = new List<Student>()
+            {
+                new Student{Id=1, Name="An", Score=8},
+                new Student{Id=2, Name="Binh", Score=6},
+                new Student{Id=3, Name="Chi", Score=9},
+                new Student{Id=4, Name="Dung", Score=7}
+            };
+            Console.WriteLine("Danh sach hoc sinh la:");
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Score: {student.Score}");
+
+            }
+            Console.WriteLine("----------------------------------------\n");
+
+            var listResult = students.Average(s => s.Score);
+            Console.WriteLine($"Diem trung binh cua sinh vien la: {listResult}");
+            Console.ReadKey();
+        }
+
+    }
+}
